79252572887cc56052fac513e93859343bc2af7c
wagner1067 <barra-wag@hotmail.com>
2026-06-30 23:02:08 -0300

    feat: initialize NestJS project with Prisma ORM, PostgreSQL adapter, and User schema configuration

---
+++ src/models/prisma/prisma.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const connectionString = process.env.DATABASE_URL;
+    const pool = new Pool({ connectionString });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onApplicationShutdown() {
+    await this.$disconnect();
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { afterEach, beforeEach, describe, it } from '@jest/globals';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
